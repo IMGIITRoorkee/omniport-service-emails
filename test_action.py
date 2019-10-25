baa0102735a7d6b5d173e62e0a10e6ce683e6f7b
Pushpam Choudhary <pchoudhary1@ee.iitr.ac.in>
2019-10-25 10:29:19 +0530

    Removed Test functions

--- test_action.py
+++ test_action.py
@@ -1,20 +1,23 @@
 from django.core.mail import EmailMessage
-from omniport.settings import settings
+from core.configuration.models.project.emails import Emails
 from kernel.models import Person
 
 from categories.redisdb import Subscription
 from emails.tasks.push_email import qpush
-from emails.sample import html_content
+from emails.html_content import html_content
 
 def email_push(
         subject,
         ):
+        em = Emails()
         msg = EmailMessage(
                 subject=subject,
                 body=html_content,
-                from_email=settings.EMAIL_HOST_USER,
+                from_email=em.EMAIL_HOST_USER,
                 to=['pchoudhary1@ee.iitr.ac.in']
         )
         msg.content_subtype = "html"
+        msg.mixed_subtype = "related"
+        msg.attach('logo_192.png', 'image/png')
         qpush(msg)
 
