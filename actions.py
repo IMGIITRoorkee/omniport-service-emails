f3ad8bd86891ae6e9acada0436f4bdc58b2c85ff
Arpit Gupta <guptarpit1997@gmail.com>
2019-12-27 12:24:57 +0530

    Add a function to get contact information and a parameter to use custom user email

--- actions.py
+++ actions.py
@@ -1,10 +1,13 @@
 from django.core.mail import EmailMessage
 from emails.html_content import html_content
-from omniport.settings import settings
+from django.conf import settings
 from kernel.models import Person
 from categories.redisdb import Subscription
 from emails.tasks.push_email import qpush
 from datetime import datetime
+
+from emails.utils.get_person_contact import get_person_contact
+
 
 def email_push(
         subject_text,
@@ -14,9 +17,10 @@ def email_push(
         has_custom_user_target=False,
         persons=None,
         target_app_name=None,
-        target_app_url=None
+        target_app_url=None,
+        use_custom_email=False
         ):
-    
+
     if target_app_name is not None and target_app_url is None:
         target_name=target_app_name
         target_url=''
@@ -40,26 +44,37 @@ def email_push(
             )
         else:
             for x in persons:
-                p = Person.objects.get(id=x)
-                msg = EmailMessage(                
-                    subject=subject_text,
-                    body=html_content.replace("Subject/Text", subject_text).replace("Body/Text", body_text).replace("Sender/Text", p.full_name).replace("TargetApp/Text", target_name).replace("TargetURL/Text", target_url).replace("Time/Text", now.strftime("%d %B %Y at %H:%M")),
-                    from_email=email_from,
-                    to=[p.contact_information.get().email_address]
-                )
-                msg.content_subtype = "html"
-                qpush(msg)
+                p = None
+                try:
+                    p = Person.objects.get(id=x)
+                except:
+                    pass
 
+                if p:
+                    msg = EmailMessage(
+                        subject=subject_text,
+                        body=html_content.replace("Subject/Text", subject_text).replace("Body/Text", body_text).replace("Sender/Text", p.full_name).replace("TargetApp/Text", target_name).replace("TargetURL/Text", target_url).replace("Time/Text", now.strftime("%d %B %Y at %H:%M")),
+                        from_email=email_from,
+                        to=[get_person_contact(p, use_custom_email)]
+                    )
+                    msg.content_subtype = "html"
+                    qpush(msg)
     else:
         category = category.slug
         persons = Subscription.fetch_people(category_slug=category, action='email')
         for x in persons:
-            p = Person.objects.get(id=x)
-            msg = EmailMessage(
-                subject=subject_text,
-                body=html_content.replace("Subject/Text", subject_text).replace("Body/Text", body_text)..replace("Sender/Text", p.full_name).replace("TargetApp/Text", target_name).replace("TargetURL/Text", target_url).replace("Time/Text", now.strftime("%d %B %Y at %H:%M")),
-                from_email=email_from,
-                to=[p.contact_information.get().email_address]
-            )
-            msg.content_subtype = "html"
-            qpush(msg)   
+            p = None
+            try:
+                p = Person.objects.get(id=x)
+            except:
+                pass
+
+            if p:
+                msg = EmailMessage(
+                    subject=subject_text,
+                    body=html_content.replace("Subject/Text", subject_text).replace("Body/Text", body_text).replace("Sender/Text", p.full_name).replace("TargetApp/Text", target_name).replace("TargetURL/Text", target_url).replace("Time/Text", now.strftime("%d %B %Y at %H:%M")),
+                    from_email=email_from,
+                    to=[get_person_contact(p, use_custom_email)]
+                )
+                msg.content_subtype = "html"
+                qpush(msg)
