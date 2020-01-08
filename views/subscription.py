b1cd90558e30615b8dd4437b4f3890db02ddd197
Shreyansh Jain <jain.shreyansh0@gmail.com>
2020-01-09 01:54:12 +0530

    Add logging

--- views/subscription.py
+++ views/subscription.py
@@ -1,3 +1,5 @@
+import logging
+
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,23 +8,29 @@ from rest_framework.views import APIView
 from categories.models import UserSubscription, Category
 from categories.serializers import SubscriptionTreeSerializer
 
+logger = logging.getLogger('emails')
+
 
 class Subscription(APIView):
 
     permission_classes = [IsAuthenticated, ]
-    
+
     def get(self, request, *args, **kwargs):
 
         roots = Category.objects.root_nodes()
         serializer = SubscriptionTreeSerializer(roots, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request, *args, **kwargs):
-      
+
         try:
             new_subscriptions = request.data['save']
             new_unsubscription = request.data['delete']
         except KeyError:
+            logger.error(
+                f'Post request sent by {self.request.person} '
+                'was identified as bad request due to \'KeyError\' exception'
+            )
             return Response(
                 data={
                     'success': False,
@@ -44,7 +52,10 @@ class Subscription(APIView):
                 category=category,
                 action='email',
             ).unsubscribe()
-
+        logger.info(
+            'Successfully updated the email subscriptions for '
+            f'{self.request.person}'
+        )
         return Response(
             data={
                 'success': True,
