98a2bb1f503bd6ca50882546f50fd5023daae2ba
anshuldutt21 <anshuldutt21@gmail.com>
2020-09-16 23:39:45 +0530

    add signals for email subscriptions for new users and categories

---
+++ signals/__init__.py
@@ -0,0 +1,7 @@
+from emails.signals.subscription_all_categories import (
+    subscription_all_categories
+)
+
+from emails.signals.subscription_all_persons import (
+    subscription_all_persons
+)
